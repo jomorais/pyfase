1fdad34b31b362018eb4084d3e4c78c0472872ea
Joaci Morais <jmorais@vtinova.com.br>
2021-06-21 14:48:33 -0400

    changes: + on_idle callback is now called on_default_state + the on_default_state can be called continuously every on_default_state_time

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyfase',
-    version='1.0.4',
+    version='1.0.5',
     url='https://github.com/jomorais/pyfase',
     license='GPLv3',
     author='Joaci Morais',
